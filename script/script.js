e0d792a6d67b15595482e1b16ec5225a5a9e646a
Billy <billy.tab@hotmail.com>
2023-11-24 14:52:48 +0100

    image

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const toggleMenuBtn = document.querySelector('#menu-btn');
+const toggleMenuImg = document.querySelector('#menu-btn img');
+const toggledMenu = document.querySelector('toggled-menu');
+const menuLinks = document.querySelector('#main-nav ul a');
+
